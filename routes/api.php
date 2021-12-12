06114911217437db38cd57ea608dcbc0f7e7849b
Yurine-Sama <Yurine2019@gmail.com>
2021-12-12 13:56:27 +0700

    ver 1.1

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+//Auth
+Route::post('/register', [\App\Http\Controllers\AuthController::class, 'register']);
+Route::post('/login', [\App\Http\Controllers\AuthController::class, 'login']);
+
+
+//Public
+Route::get('/product', [\App\Http\Controllers\ProductController::class, 'index']);
+Route::get('/product/show/{id}', [\App\Http\Controllers\ProductController::class, 'show']);
+Route::get('/product/search/{name}', [\App\Http\Controller\ProductController::class, 'search']);
+//Protect
+Route::middleware('auth:sanctum')->post('/product/store', [\App\Http\Controllers\ProductController::class, 'store']);
+Route::middleware('auth:sanctum')->post('/product/update', [\App\Http\Controllers\ProductController::class, 'update']);
+Route::delete('/product/{id}', [\App\Http\Controller\ProductController::class, 'delete']);
+
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
